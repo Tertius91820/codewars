721c429c25ba39851889e9e21704950930613978
tertius Jubber <tjubber@gmail.com>
2022-10-06 15:01:33 +0200

    added

---
+++ trueFalse.js
@@ -0,0 +1,13 @@
+//Paramaters - true or false as a input 
+
+//Return - stringify input
+
+//Example - true => "true"
+//         false =>  "false"
+
+//PsuesoCode -Stringyfy inputs 
+
+
+function booleanToString(b){
+  return b == true ? "true" : "false"
+  }console.log(booleanToString(false))
